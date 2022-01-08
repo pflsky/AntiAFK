5bc38bc32d8db85b4133705bf71bf18662f3ffdf
flasky <87421383+pflsky@users.noreply.github.com>
2022-01-08 19:09:26 +0000

    Add files via upload

--- src/bot.java
+++ src/bot.java
@@ -12,17 +12,17 @@ public class bot {
     //more presets might come soon, these are the main games I play so these were the most convenient.
 
     public void decide() {
-        System.out.println("pick preset: (valorant, rocket league, minecraft)");
+        System.out.println("pick preset: (v, rl, mc)");
         String preset = scanner.nextLine();
 
         switch(preset) {
-            case "valorant": System.out.println("loading preset " + preset);
+            case "v": System.out.println("loading preset " + preset);
                 new presets.valorant();
                 break;
-            case "rocket league": System.out.println("loading preset " + preset);
+            case "rl": System.out.println("loading preset " + preset);
                 new presets.rocketLeague();
                 break;
-            case "minecraft": System.out.println("loading preset " + preset);
+            case "mc": System.out.println("loading preset " + preset);
                 new presets.minecraft();
                 break;
             default: decide();
--- src/presets/rocketLeague.java
+++ src/presets/rocketLeague.java
@@ -2,7 +2,6 @@ package presets;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.security.Key;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,9 +9,10 @@ public class rocketLeague {
 
     Scanner scanner = new Scanner(System.in);
 
-    Robot r;
+    Random random = new Random();
+    int randomNumber;
 
-    {
+    Robot r; {
         try {
             r = new Robot();
         } catch (AWTException e) {
@@ -21,270 +21,64 @@ public class rocketLeague {
     }
 
     public rocketLeague() {
-
-        System.out.println("do you want to look like a bot? (y/n)");
+        System.out.println("Chat mode or mad mode? (c/m)");
         String decision = scanner.nextLine();
-
-        if (decision.equals("y")) {
-            new bot();
-        } else if (decision.equals("n")) {
-            new notBot();
+        if (decision.equals("c")) {
+            new chatMode();
+        } else if (decision.equals("m")) {
+            new madMode();
         } else {
-            new rocketLeague();
+            System.out.println("that is not a mode, try again");
         }
     }
 
-    public class notBot {
+    public class chatMode {
 
-        Random random = new Random();
-        int randomNumber;
+        public chatMode() {
+            sayMsg();
+            r.delay(10000);
+            new chatMode();
+        }
 
-        public notBot() {
+        public void sayMsg() {
+            r.keyPress(KeyEvent.VK_1);
+            r.keyRelease(KeyEvent.VK_1);
+            r.keyPress(KeyEvent.VK_1);
+            r.keyRelease(KeyEvent.VK_1);
+        }
+    }
 
-            changeRandom();
+    public class madMode {
 
+        public madMode() {
+            randomNumber = random.nextInt(17);
             switch(randomNumber) {
-                case 1:
-                    r.keyPress(KeyEvent.VK_W);
-                    break;
-                case 2:
-                    r.keyRelease(KeyEvent.VK_W);
-                    break;
-                case 3:
-                    r.keyPress(KeyEvent.VK_A);
-                    break;
-                case 4:
-                    r.keyRelease(KeyEvent.VK_A);
-                    break;
-                case 5:
-                    r.keyPress(KeyEvent.VK_S);
-                    break;
-                case 6:
-                    r.keyRelease(KeyEvent.VK_S);
-                    break;
-                case 7:
-                    r.keyPress(KeyEvent.VK_D);
-                    break;
-                case 8:
-                    r.keyRelease(KeyEvent.VK_D);
-                    break;
-                case 9:
-                    r.keyPress(KeyEvent.VK_SPACE);
-                    break;
-                case 10:
-                    r.keyRelease(KeyEvent.VK_SPACE);
-                    break;
-                case 11:
-                    r.keyPress(KeyEvent.VK_SHIFT);
-                    break;
-                case 12:
-                    r.keyRelease(KeyEvent.VK_SHIFT);
-                    break;
-                case 13:
-                    r.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
-                    break;
-                case 14:
-                    r.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
-                    break;
-                case 15:
-                    r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-                    break;
-                case 16:
-                    r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-                    break;
+                case 1: r.keyPress(KeyEvent.VK_W); break;
+                case 2: r.keyRelease(KeyEvent.VK_W); break;
+                case 3: r.keyPress(KeyEvent.VK_A); break;
+                case 4: r.keyRelease(KeyEvent.VK_A); break;
+                case 5: r.keyPress(KeyEvent.VK_S); break;
+                case 6: r.keyRelease(KeyEvent.VK_S); break;
+                case 7: r.keyPress(KeyEvent.VK_D); break;
+                case 8: r.keyRelease(KeyEvent.VK_D); break;
+                case 9: r.keyPress(KeyEvent.VK_SPACE); break;
+                case 10: r.keyRelease(KeyEvent.VK_SPACE); break;
+                case 11: r.keyPress(KeyEvent.VK_SHIFT); break;
+                case 12: r.keyRelease(KeyEvent.VK_SHIFT); break;
+                case 13: r.mousePress(KeyEvent.BUTTON1_DOWN_MASK); break;
+                case 14: r.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK); break;
+                case 15: r.mousePress(KeyEvent.BUTTON2_DOWN_MASK); break;
+                case 16: r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK); break;
             }
             r.delay(1000);
             new rocketLeague();
-        }
-
-        public void changeRandom() {
-            randomNumber = random.nextInt(17);
         }
     }
 
     public class bot {
 
-        Random random = new Random();
-        int time = random.nextInt(6000);
-        int randomNumber;
-
         public bot() {
-            changeRandom();
-            switch(randomNumber) {
-                case 1: driveForward(time); break;
-                case 2: reverse(time); break;
-                case 3: driveLeft(time); break;
-                case 4: driveRight(time); break;
-                case 5: reverseLeft(time); break;
-                case 6: reverseRight(time); break;
-                case 7: doubleJump(); break;
-                case 8: jump(); break;
-                case 9: frontflip(); break;
-                case 10: backflip(); break;
-                case 11: leftFlip(); break;
-                case 12: rightFlip(); break;
-                case 13: diagLeft(); break;
-                case 14: diagRight(); break;
-                case 15: aerial(time); break;
-            }
-        }
 
-        public void changeRandom() {
-            randomNumber = random.nextInt(16);
-        }
-
-        public void driveForward(int time) {
-            r.keyPress(KeyEvent.VK_W);
-            r.delay(time);
-            r.keyRelease(KeyEvent.VK_W);
-            new bot();
-        }
-
-        public void driveLeft(int time) {
-            r.keyPress(KeyEvent.VK_W);
-            r.keyPress(KeyEvent.VK_A);
-            r.delay(time);
-            r.keyRelease(KeyEvent.VK_W);
-            r.keyRelease(KeyEvent.VK_A);
-            new bot();
-        }
-
-        public void driveRight(int time) {
-            r.keyPress(KeyEvent.VK_W);
-            r.keyPress(KeyEvent.VK_D);
-            r.delay(time);
-            r.keyRelease(KeyEvent.VK_W);
-            r.keyRelease(KeyEvent.VK_D);
-            new bot();
-        }
-
-        public void reverse(int time) {
-            r.keyPress(KeyEvent.VK_S);
-            r.delay(time);
-            r.keyRelease(KeyEvent.VK_S);
-            new bot();
-        }
-
-        public void reverseLeft(int time) {
-            r.keyPress(KeyEvent.VK_S);
-            r.keyPress(KeyEvent.VK_A);
-            r.delay(time);
-            r.keyRelease(KeyEvent.VK_S);
-            r.keyRelease(KeyEvent.VK_A);
-            new bot();
-        }
-
-        public void reverseRight(int time) {
-            r.keyPress(KeyEvent.VK_S);
-            r.keyPress(KeyEvent.VK_D);
-            r.delay(time);
-            r.keyRelease(KeyEvent.VK_S);
-            r.keyRelease(KeyEvent.VK_D);
-            new bot();
-        }
-
-        public void doubleJump() {
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            new bot();
-        }
-
-        public void jump() {
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            new bot();
-        }
-
-        public void frontflip() {
-            r.keyPress(KeyEvent.VK_W);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.keyRelease(KeyEvent.VK_W);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            new bot();
-        }
-
-        public void backflip() {
-            r.keyPress(KeyEvent.VK_S);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.keyRelease(KeyEvent.VK_S);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            new bot();
-        }
-
-        public void leftFlip() {
-            r.keyPress(KeyEvent.VK_D);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.keyRelease(KeyEvent.VK_D);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            new bot();
-        }
-
-        public void rightFlip() {
-            r.keyPress(KeyEvent.VK_A);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.keyRelease(KeyEvent.VK_A);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            new bot();
-        }
-
-        public void diagLeft() {
-            r.keyPress(KeyEvent.VK_W);
-            r.keyPress(KeyEvent.VK_A);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.keyRelease(KeyEvent.VK_W);
-            r.keyRelease(KeyEvent.VK_A);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            new bot();
-        }
-
-        public void diagRight() {
-            r.keyPress(KeyEvent.VK_W);
-            r.keyPress(KeyEvent.VK_D);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            r.mousePress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.keyRelease(KeyEvent.VK_W);
-            r.keyRelease(KeyEvent.VK_D);
-            r.mouseRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            new bot();
-        }
-
-        public void aerial(int time) {
-            r.keyPress(KeyEvent.VK_S);
-            r.keyPress(KeyEvent.BUTTON2_DOWN_MASK);
-            r.delay(500);
-            r.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
-            r.delay(time);
-            r.keyRelease(KeyEvent.VK_S);
-            r.keyRelease(KeyEvent.BUTTON2_DOWN_MASK);
-            r.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
         }
     }
 }
